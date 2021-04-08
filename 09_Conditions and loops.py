ca752717bdf25ef30ff68b49f82e0fb4b45141da
Igor Srdoc <igor@srdoc.si>
2021-04-08 17:57:44 +0200

    Add hint to secret number game

--- 09_Conditions and loops.py
+++ 09_Conditions and loops.py
@@ -7,23 +7,32 @@ for x in range(5):
         print("You've guessed it - congratulations! It's number 22.")
         break
 
-    print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+    if guess > secret:
+        print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
+
+print("End for loop")
 
 
-# secret = 22
-# guess = None
+secret = 22
+guess = None
 
-# did_guess_secret = False
+did_guess_secret = False
 
-# while not did_guess_secret: # While True
-#   # Do all this
-#    guess = int(input("Guess the secret number (between 1 and 30): "))
-#    did_guess_secret = guess == secret
-#
-#    if did_guess_secret:
-#        print("You guessed it - congratulations! It's number 22.")
-#    else:
-#        print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+while not did_guess_secret: # While True
+    # Do all this
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+    did_guess_secret = guess == secret
 
-# print("End")
+    if did_guess_secret:
+        print("You guessed it - congratulations! It's number 22.")
+        break
 
+    if guess > secret:
+        print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+        print("Your guess is not correct... try something bigger")
+
+
+print("End")
