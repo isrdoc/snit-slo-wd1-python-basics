0db7e8a836fb2acd0f579b3b1273e49df44a3c15
Igor Srdoc <igor@srdoc.si>
2021-04-08 18:20:49 +0200

    Add random number and use string formatting for variables in strings

--- 09_Conditions and loops.py
+++ 09_Conditions and loops.py
@@ -1,32 +1,20 @@
-secret = 22
+from random import randint
 
-for x in range(5):
-    guess = int(input("Guess the secret number (between 1 and 30): "))
+minimum = 1
+maximum = 30
 
-    if guess == secret:
-        print("You've guessed it - congratulations! It's number 22.")
-        break
-
-    if guess > secret:
-        print("Your guess is not correct... try something smaller")
-    elif guess < secret:
-        print("Your guess is not correct... try something bigger")
-
-print("End for loop")
-
-
-secret = 22
+secret = randint(minimum, maximum)
 guess = None
 
 did_guess_secret = False
 
 while not did_guess_secret: # While True
     # Do all this
-    guess = int(input("Guess the secret number (between 1 and 30): "))
+    guess = int(input(f"Guess the secret number (between {minimum} and {maximum}): "))
     did_guess_secret = guess == secret
 
     if did_guess_secret:
-        print("You guessed it - congratulations! It's number 22.")
+        print(f"You guessed it - congratulations! It's number {secret}.")
         break
 
     if guess > secret:
