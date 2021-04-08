673faa9b0c5d51fd8dafea3301060a2126dfe9d1
Igor Srdoc <igor@srdoc.si>
2021-04-08 17:24:41 +0200

    Add for loop example with friends

--- 09_Conditions and loops.py
+++ 09_Conditions and loops.py
@@ -1,17 +1,22 @@
-secret = 22
-guess = None
+# secret = 22
+# guess = None
 
-did_guess_secret = False
+# did_guess_secret = False
 
-while not did_guess_secret: # While True
-    # Do all this
-    guess = int(input("Guess the secret number (between 1 and 30): "))
-    did_guess_secret = guess == secret
+friends = ["Janko", "Metka", "Jure", "Mara", "Mojca", "Marko"]
 
-    if did_guess_secret:
-        print("You guessed it - congratulations! It's number 22.")
-    else:
-        print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+for friend in friends:
+    print(friend + " is my friend.")
 
-print("End")
+# while not did_guess_secret: # While True
+#   # Do all this
+#    guess = int(input("Guess the secret number (between 1 and 30): "))
+#    did_guess_secret = guess == secret
+#
+#    if did_guess_secret:
+#        print("You guessed it - congratulations! It's number 22.")
+#    else:
+#        print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+
+# print("End")
 
