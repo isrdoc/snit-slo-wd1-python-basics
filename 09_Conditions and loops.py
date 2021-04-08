fd8f70a05c569a4cdb7d88e25f1bb9c83cce6d57
Igor Srdoc <igor@srdoc.si>
2021-04-08 17:39:10 +0200

    Add for loop for secret number

--- 09_Conditions and loops.py
+++ 09_Conditions and loops.py
@@ -1,12 +1,19 @@
+secret = 22
+
+for x in range(5):
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+
+    if guess == secret:
+        print("You've guessed it - congratulations! It's number 22.")
+        break
+
+    print("Sorry, your guess is not correct... The secret number is not " + str(guess))
+
+
 # secret = 22
 # guess = None
 
 # did_guess_secret = False
-
-friends = ["Janko", "Metka", "Jure", "Mara", "Mojca", "Marko"]
-
-for friend in friends:
-    print(friend + " is my friend.")
 
 # while not did_guess_secret: # While True
 #   # Do all this
